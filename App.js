b7704928cadb059c297ade4c54fa37068e92adb3
jennyhuoh <39110160+jennyhuoh@users.noreply.github.com>
2020-03-12 17:49:30 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>110619040</Text>
+      <Text>霍佳立ya</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffe6fe',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
